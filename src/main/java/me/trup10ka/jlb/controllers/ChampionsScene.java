2f87a06d8b6992cf509bb8faf0c586d3291a53f0
Lukas Friedl <lukys3036@gmail.com>
2023-04-09 18:55:04 +0200

    Added ChampionsScene + class which makes images rounded

---
+++ src/main/java/me/trup10ka/jlb/controllers/ChampionsScene.java
@@ -0,0 +1,70 @@
+package me.trup10ka.jlb.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressIndicator;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import me.trup10ka.jlb.app.JLB;
+import me.trup10ka.jlb.data.Champion;
+import me.trup10ka.jlb.util.RoundCorners;
+import me.trup10ka.jlb.web.parser.HtmlChampionsPageParser;
+
+import java.util.ArrayList;
+
+public class ChampionsScene {
+    private static ChampionsScene instance;
+    private HtmlChampionsPageParser championsPageParser;
+
+    @FXML
+    private ProgressIndicator progressIndicator;
+    @FXML
+    private FlowPane championsPane;
+
+    public ChampionsScene() {
+        instance = this;
+    }
+
+    public void setChampionsPageParser(HtmlChampionsPageParser championsPageParser) {
+        this.championsPageParser = championsPageParser;
+        this.progressIndicator.setVisible(false);
+        fillChampionsPane();
+    }
+    public void fillChampionsPane() {
+        ArrayList<Champion> champions = championsPageParser.champions();
+        for (Champion champion : champions) {
+            addToTilePane(champion);
+        }
+    }
+    private void addToTilePane(Champion champion) {
+        StackPane imgViewPane = new StackPane();
+        imgViewPane.getStyleClass().add("champion-image");
+
+        ImageView imageView = new ImageView("champions/" + champion.getName().toLowerCase().replaceAll("[. ]", "") + ".png");
+        imageView.setFitHeight(60); imageView.setFitWidth(60);
+        RoundCorners.setRoundCorners(imageView);
+
+        imgViewPane.getChildren().add(imageView);
+        Label nameLabel = new Label(champion.getName());
+        nameLabel.getStyleClass().add("champion-name");
+
+        FlowPane flowPane = new FlowPane();
+        flowPane.getStyleClass().add("champion-card");
+
+        flowPane.getChildren().add(imgViewPane);
+        flowPane.getChildren().add(nameLabel);
+        flowPane.setOnMouseClicked(mouseEvent -> {
+            JLB.getInstance().switchToBuildScene();
+            BuildScene.getInstance().setChampionToParse(champion.getName().toLowerCase().replaceAll("[. ]", ""));
+        });
+
+        this.championsPane.getChildren().add(flowPane);
+    }
+    public static ChampionsScene getInstance() {
+        return instance;
+    }
+
+    public void executeErrorLabel(Exception exception) {
+        System.out.println(exception.getMessage());
+    }
+}
---
+++ src/main/java/me/trup10ka/jlb/util/RoundCorners.java
@@ -0,0 +1,27 @@
+package me.trup10ka.jlb.util;
+
+import javafx.scene.SnapshotParameters;
+import javafx.scene.image.ImageView;
+import javafx.scene.image.WritableImage;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+
+
+public class RoundCorners {
+    public static void setRoundCorners(ImageView imageView) {
+        Rectangle clip = new Rectangle(
+                imageView.getFitWidth(), imageView.getFitHeight()
+        );
+        clip.setArcWidth(15); clip.setArcHeight(15);
+        imageView.setClip(clip);
+
+        SnapshotParameters parameters = new SnapshotParameters();
+        parameters.setFill(Color.TRANSPARENT);
+        WritableImage image = imageView.snapshot(parameters, null);
+
+        imageView.setClip(null);
+        imageView.setImage(image);
+    }
+
+}
