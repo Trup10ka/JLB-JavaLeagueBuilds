07c27d998a0451cdf79f0c2ceb704fad489640fc
Lukas Friedl <lukys3036@gmail.com>
2023-04-10 19:13:52 +0200

    Main added

---
+++ src/main/java/me/trup10ka/jlb/app/Main.java
@@ -0,0 +1,14 @@
+package me.trup10ka.jlb.app;
+
+import javafx.application.Application;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        /*TestClass testClass = new TestClass();
+        testClass.createNewChampion();*/
+
+        Application.launch(JavaLeagueBuilds.class, args);
+
+    }
+}
