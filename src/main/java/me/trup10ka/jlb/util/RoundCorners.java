93bed9f70ad93b66c65920740578b1c5f3d5b2d1
Lukas Friedl <lukys3036@gmail.com>
2023-04-14 01:00:38 +0200

    Added arrow icon to move out from build scene back to champions scene - round corners for items

--- src/main/java/me/trup10ka/jlb/util/RoundCorners.java
+++ src/main/java/me/trup10ka/jlb/util/RoundCorners.java
@@ -3,6 +3,7 @@ package me.trup10ka.jlb.util;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.WritableImage;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
@@ -22,5 +23,10 @@ public class RoundCorners {
 
         imageView.setClip(null);
         imageView.setImage(image);
+    }
+    public static void setRoundedCornerToItem(Pane pane) {
+        Rectangle rectangle = new Rectangle(48, 48);
+        rectangle.setArcWidth(15); rectangle.setArcHeight(15);
+        pane.setClip(rectangle);
     }
 }
